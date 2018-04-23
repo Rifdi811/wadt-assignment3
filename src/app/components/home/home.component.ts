6a65e070a37747b318b5a74d4df7dd00fae494c2
Bahit Hamid <bahit.hamid@gmail.com>
2018-04-23 09:26:15 +0800

    removed sample code and replaced with bootstrap3 example

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+// Below is needed to use bootstrap3 modal
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  public modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  // The bootstrap3 modal logic - tutorial into implementing bootstrap3 and angular:
+  // https://loiane.com/2017/08/how-to-add-bootstrap-to-an-angular-cli-project/
+  public openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
+  ngOnInit() {}
 }
