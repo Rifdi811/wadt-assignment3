b0ea8293546178b0916061e79cd330515305af09
Bahit Hamid <bahit.hamid@gmail.com>
2018-04-27 10:21:39 +0800

    Add UI and validation for adding group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import 'reflect-metadata';
 import '../polyfills';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 
@@ -45,6 +45,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     TranslateModule.forRoot({
--- src/app/components/groups/groups.component.ts
+++ src/app/components/groups/groups.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { Group } from './../../model/group.model';
 import { AttendanceService } from './../../attendance.service';
+import { AlertComponent } from "ngx-bootstrap/alert/alert.component";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { BsModalService } from "ngx-bootstrap/modal";
+import { BsModalRef } from "ngx-bootstrap/modal/bs-modal-ref.service";
 
 @Component({
   selector: "app-groups",
@@ -11,11 +15,83 @@ import { AttendanceService } from './../../attendance.service';
 export class GroupsComponent implements OnInit {
   groups$: Observable<Group[]>;
 
-  constructor(private groupService: AttendanceService) {}
+  groupForm: FormGroup;
+
+  // Used by AlertComponent
+  alerts: any[] = [];
+
+  modalRef: BsModalRef;
+  message: string;
+
+  constructor(
+    private groupService: AttendanceService,
+    private modalService: BsModalService
+  ) {}
 
   ngOnInit() {
+    // Form validation, Submit button will only work if all valid
+    this.groupForm = new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(5)]),
+      quantity: new FormControl("", [
+        Validators.required,
+        Validators.pattern("^[0-9]*$")
+      ]),
+      gc: new FormControl("", Validators.required),
+      gl: new FormControl("", Validators.required)
+    });
+
     this.groups$ = this.groupService.getCollection$(ref =>
       ref.orderBy("name", "asc")
     );
+  }
+
+  // Saving group
+  save() {
+    // Taking values from input HTML
+    const name = this.groupForm.get("name").value;
+    const quantity = this.groupForm.get("quantity").value;
+    const gc = this.groupForm.get("gc").value;
+    const gl = this.groupForm.get("gl").value;
+    // sending off to service to save
+    this.groupService.add({ name, quantity, gc, gl });
+    this.add(); // Show confirmation
+    this.groupForm.reset();
+  }
+
+  // Delete Group
+  remove(id: string) {
+    this.groupService.remove(id);
+    this.modalRef.hide();
+    this.removedMessage();
+  }
+
+  // Alert stuff, such as success in adding new data into DB
+  add(): void {
+    this.alerts.push({
+      type: "success",
+      msg: "Saved group into database",
+      timeout: 3000 // in miliseconds 1000 is 1 second
+    });
+  }
+
+  removedMessage(): void {
+    this.alerts.push({
+      type: "warning",
+      msg: "Removed group from database",
+      timeout: 3000 // in miliseconds 1000 is 1 second
+    });
+  }
+
+  onClosed(dismissedAlert: AlertComponent): void {
+    this.alerts = this.alerts.filter(alert => alert !== dismissedAlert);
+  }
+
+  // Modal stuff
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: "modal-sm" });
+  }
+
+  decline(): void {
+    this.modalRef.hide();
   }
 }
