d1368b2e6e55284dabc0d99e9607d62bc653a0e3
Bahit Hamid <bahit.hamid@gmail.com>
2018-04-30 10:03:45 +0800

    Add routing to new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { ListGlComponent } from './components/list-gl/list-gl.component';
+import { ListModulesComponent } from './components/list-modules/list-modules.component';
+import { AttendancesLecturerViewComponent } from './components/attendances-lecturer-view/attendances-lecturer-view.component';
+import { AttendanceSingleViewComponent } from './components/attendance-single-view/attendance-single-view.component';
 import { AttendancesComponent } from './components/attendances/attendances.component';
 import { GroupsComponent } from './components/groups/groups.component';
 import { TestComponent } from './components/test/test.component';
@@ -13,8 +17,12 @@ const routes: Routes = [
     component: HomeComponent
   },
   { path: "test", component: TestComponent },
-  { path: "groups", component:  GroupsComponent},
-  { path: "attendances", component:  AttendancesComponent}
+  { path: "groups", component: GroupsComponent },
+  { path: "attendances", component: AttendancesComponent },
+  { path: "attendances/:id", component: AttendanceSingleViewComponent },
+  { path: "attendancesLecturerView", component: AttendancesLecturerViewComponent },
+  { path: "modules", component: ListModulesComponent },
+  { path: "gl", component: ListGlComponent },
 ];
 
 @NgModule({
